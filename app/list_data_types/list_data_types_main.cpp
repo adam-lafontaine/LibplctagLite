23c42161ebbd2caf937305045e6ea3c740d06f01
adam-lafontaine <adam.lafontaine@gmail.com>
2023-10-20 08:32:48 -0400

    print tag data types

--- app/list_data_types/list_data_types_main.cpp
+++ app/list_data_types/list_data_types_main.cpp
@@ -3,16 +3,35 @@
 #include <cstdio>
 
 
-
-
 int main()
 {
 	auto data = plcscan::init();
 
-	for (auto const& type : data.data_types)
+	auto& types = data.data_types;
+
+	if (types.empty())
 	{
-		printf("%s: %s\n", type.data_type_name.begin, type.data_type_description.begin);
+		printf("Error. No PLC data types found.\n");
+		return 1;
+	}
+
+	auto max_len = types[0].data_type_name.length;
+	for (auto const& type : types)
+	{
+		if (type.data_type_name.length > max_len)
+		{
+			max_len = type.data_type_name.length;
+		}
+	}
+
+	max_len++;
+
+	for (auto const& type : types)
+	{
+		printf("%*s: %s\n", (int)max_len, type.data_type_name.begin, type.data_type_description.begin);
 	}
 
 	plcscan::disconnect();
+
+	return 0;
 }
