2a524d8035a530d9ed055caecddf773a8fb5a96c
adam-lafontaine <adam.lafontaine@gmail.com>
2023-10-27 11:37:01 -0400

    tag value and size

--- src/dev/devplctag.cpp
+++ src/dev/devplctag.cpp
@@ -1,6 +1,12 @@
 #include "devplctag.hpp"
+#include "../util/memory_helper.hpp"
 
-#include "../util/mh_types.hpp"
+#include <vector>
+
+template <typename T>
+using List = std::vector<T>;
+
+namespace mh = memory_helper;
 
 
 namespace dev
@@ -28,7 +34,31 @@ namespace dev
             );
         }
     };
+
+
+    class TagValue
+    {
+    public:
+        ByteView value_bytes;
+    };
 }
+
+
+namespace
+{
+    class TagDatabase
+    {
+    public:
+
+        List<dev::TagEntry> tag_entries;
+        List<dev::TagValue> tag_values;
+
+        ByteBuffer tag_value_bytes;
+    };
+}
+
+
+static TagDatabase g_tag_db{};
 
 
 /* api */
@@ -37,30 +67,72 @@ namespace dev
 {
     int plc_tag_create(const char* attr, int timeout)
     {
-        return -1;
+        if (attr[0] == '@')
+        {
+            if (attr[1] == 't')
+            {
+                // generate tag listing
+            }
+            else if (attr[1] == 'u')
+            {
+                // generate udt entry
+            }
+
+            return -1;
+        }
+
+        // create a tag
+        // name matches one in the listing
+
+        return -1; // PLCTAG_STATUS_OK;
     }
 
 
     int plc_tag_read(int handle, int timeout)
     {
-        return -1;
+        // generate a new value for the tag
+        return -1; // PLCTAG_STATUS_OK;
     }
 
 
     int plc_tag_get_size(int handle)
     {
-        return -1;
+        auto& tags = g_tag_db.tag_values;
+
+        if (handle < 0 || (u64)handle >= tags.size())
+        {
+            return -1;
+        }
+
+        auto size = (int)tags[handle].value_bytes.length;
+
+        return size ? size : -1;
     }
 
 
     int plc_tag_get_raw_bytes(int handle, int offset, unsigned char* dst, int length)
     {
-        return -1;
+        auto& tags = g_tag_db.tag_values;
+
+        if (handle < 0 || (u64)handle >= tags.size())
+        {
+            return -1;
+        }
+
+        auto& value = tags[handle].value_bytes;
+
+        auto len = (u32)length < value.length ? (u32)length : value.length;
+
+        auto src = value.data;
+
+        mh::copy_bytes(src, dst, len);
+
+        return PLCTAG_STATUS_OK;
     }
 
 
     void plc_tag_shutdown()
     {
-        
+        mb::destroy_buffer(g_tag_db.tag_value_bytes);
     }
 }
